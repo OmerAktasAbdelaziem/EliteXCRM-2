97e636097cdc9ac3be02e03279721a037a94f14b
OmerAktasAbdelaziem <contact.us.omer@gmail.com>
2025-08-11 12:19:55 +0300

    rtel

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -432,6 +432,7 @@ $clientService = app(ClientServiceInterface::class);
 
         if (isset($update['message'])) {
             $chat_id = $update['message']['chat']['id'];
+            $this->sendNotification($chat_id, "✅ $type Request #$id has been accepted.");
             $telegramChat = TelegramChat::where('type','notifi')->find($chat_id);
             if ($telegramChat) {
                 $text = $update['message']['text'] ?? '';
