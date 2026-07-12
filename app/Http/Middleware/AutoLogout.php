39c22f6298bebdab93f323fd416851a6f65facd5
DESKTOP-0OLL37O\anb_s <anb_syria@hotmail.com>
2026-07-12 15:37:47 +0300

    Amer

--- app/Http/Middleware/AutoLogout.php
+++ app/Http/Middleware/AutoLogout.php
@@ -17,12 +17,12 @@ class AutoLogout
             if ($lastActivity) {
                 $inactive = $now->diffInSeconds(\Carbon\Carbon::parse($lastActivity));
 
-                /*if ($inactive > 1800) {
+                if ($inactive > 28800) {
                     Auth::logout();
                     $request->session()->invalidate();
                     $request->session()->regenerateToken();
-                    return redirect('/login')->with('status', 'تم تسجيل خروجك تلقائياً بسبب عدم النشاط لأكثر من 30 دقيقة');
-                }*/
+                    return redirect('/login')->with('status', 'تم تسجيل خروجك تلقائياً بسبب عدم النشاط لوقت طويل');
+                }
             }
 
             session(['last_activity_time' => $now]);
