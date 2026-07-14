62dabd406820283b6dd2ac3ca2540cb6ef79770b
DESKTOP-0OLL37O\anb_s <anb_syria@hotmail.com>
2026-07-14 12:33:23 +0300

    Amer

--- app/Http/Middleware/AutoLogout.php
+++ app/Http/Middleware/AutoLogout.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -26,49 +26,6 @@ class AutoLogout
             }
 
             session(['last_activity_time' => $now]);
-        }
-
-        return $next($request);
-    }
-}*/
-
-
-namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
-
-class AutoLogout
-{
-    public function handle(Request $request, Closure $next)
-    {
-        if (Auth::check()) {
-            $lastActivity = session('last_activity_time');
-            $currentTime = time(); 
-
-            if ($lastActivity) {
-                $inactiveSeconds = $currentTime - $lastActivity;
-//28800
-                if ($inactiveSeconds > 3600 ) { // 8 ساعات  
-                    Auth::logout();
-                    $request->session()->invalidate();
-                    $request->session()->regenerateToken();
-                    
-                    // إذا كان الطلب من أجاكس، لا تفعل Redirect عادي، بل أرسل استجابة 401
-                    if ($request->ajax() || $request->wantsJson()) {
-                        return response()->json(['message' => 'Session timeout.'], 401);
-                    }
-
-                    // التوجيه العادي للطلبات العادية
-                    return redirect('/login')->with('status', 'تم تسجيل خروجك تلقائياً بسبب عدم النشاط لوقت طويل.');
-                }
-            }
-
-            // لا نحدث وقت النشاط إذا كان الطلب أجاكس لكي لا يتجدد الوقت تلقائياً
-            if (!$request->ajax() && !$request->wantsJson()) {
-                session(['last_activity_time' => $currentTime]);
-            }
         }
 
         return $next($request);
